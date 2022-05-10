a8da99939dac8ca9b5cf4f1f64c864d856062855
Obeid <hassoeni.obeid@vestia.nl>
2022-05-10 16:13:02 +0200

    add dynamic get route

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.get('/users', function (req, res) {
 	})
 })
 
+app.get('/users/:id', function (req, res) {
+    console.log(req.params.id)	
+    res.json({
+        success: true,
+        message: 'Successfully got user!',
+        user: req.params.id,
+    })
+})
+
 app.listen(8000, function () {
 	console.log('Server is running on port 8000')
 })
